037107732bb346ba42eb9921da75b2d647648c34
Adam Kowalski <adam.kowalski@miinto.com>
2020-02-18 09:45:19 +0100

    Update connection test

--- test/Rabbitmq/Connection/ConnectionTest.php
+++ test/Rabbitmq/Connection/ConnectionTest.php
@@ -10,28 +10,28 @@ namespace Maleficarum\Rabbitmq\Tests\Connection;
 class ConnectionTest extends \Maleficarum\Tests\TestCase {
     /* ------------------------------------ Method: __construct START ---------------------------------- */
     public function testConstructParameres() {
-        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 'bar', 0, 'baz', 'qux', 'aku']);
-        $this->assertSame('foo', $this->getProperty($connection, 'queueName'));
-        $this->assertSame('bar', $this->getProperty($connection, 'host'));
+        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 0, 'bar', 'baz', 'qux', 'quux', 'quuz']);
+        $this->assertSame('foo', $this->getProperty($connection, 'host'));
         $this->assertSame(0, $this->getProperty($connection, 'port'));
-        $this->assertSame('bar', $this->getProperty($connection, 'host'));
-        $this->assertSame('baz', $this->getProperty($connection, 'username'));
-        $this->assertSame('qux', $this->getProperty($connection, 'password'));
-        $this->assertSame('aku', $this->getProperty($connection, 'vhost'));
+        $this->assertSame('bar', $this->getProperty($connection, 'username'));
+        $this->assertSame('baz', $this->getProperty($connection, 'password'));
+        $this->assertSame('qux', $this->getProperty($connection, 'vhost'));
+        $this->assertSame('quux', $this->getProperty($connection, 'exchangeName'));
+        $this->assertSame('quuz', $this->getProperty($connection, 'queueName'));
     }
     
     /* ------------------------------------ Method: __construct END ------------------------------------ */
     
     /* ------------------------------------ Method: connect START -------------------------------------- */
     public function testConnectAfterItWasCalled() {
-        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 'bar', 0, 'baz', 'qux']);
+        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 0, 'bar', 'baz', 'qux', 'quux', 'quuz']);
         $connection->connect();
         
         $this->assertInstanceOf('PhpAmqpLib\Connection\AMQPStreamConnection', $connection->getConnection());
     }
     
     public function testConnectBeforeItWasCalled() {
-        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 'bar', 0, 'baz', 'qux']);
+        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 0, 'bar', 'baz', 'qux', 'quux', 'quuz']);
 
         $this->assertNull($connection->getConnection());
     }
@@ -39,7 +39,7 @@ class ConnectionTest extends \Maleficarum\Tests\TestCase {
 
     /* ------------------------------------ Method: getChannel START ----------------------------------- */
     public function testGetChannelWithCorrectId() {
-        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 'bar', 0, 'baz', 'qux']);
+        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 0, 'bar', 'baz', 'qux', 'quux', 'quuz']);
         $connection->connect();
         
         $this->assertInstanceOf('PhpAmqpLib\Channel\AMQPChannel', $connection->getChannel(1));
@@ -50,7 +50,7 @@ class ConnectionTest extends \Maleficarum\Tests\TestCase {
      * @expectedException \InvalidArgumentException
      */
     public function testGetChannelWithIncorrectId() {
-        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 'bar', 0, 'baz', 'qux']);
+        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 0, 'bar', 'baz', 'qux', 'quux', 'quuz']);
         $connection->connect();
         $connection->getChannel(-11);
     }
@@ -59,7 +59,7 @@ class ConnectionTest extends \Maleficarum\Tests\TestCase {
     
     /* ------------------------------------ Method: __destruct START ----------------------------------- */
     public function testDestruct() {
-        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 'bar', 0, 'baz', 'qux']);
+        $connection = \Maleficarum\Ioc\Container::get('Maleficarum\Rabbitmq\Connection\Connection', ['foo', 0, 'bar', 'baz', 'qux', 'quux', 'quuz']);
         $connection->connect();
         unset($connection);
     }
